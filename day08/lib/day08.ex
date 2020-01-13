f8b9c4be09522bd3af56d6556e786f59d2298a08
Matthew Boehlig <thetamind@gmail.com>
2020-01-13 02:38:08 -0600

    Day 08 part 1 solution

--- day08/lib/day08.ex
+++ day08/lib/day08.ex
@@ -3,11 +3,36 @@ defmodule Day08 do
   Documentation for `Day08`.
   """
 
+  def part1(data, width: width, height: height) do
+    data
+    |> decode(width: width, height: height)
+    |> count_digits()
+    |> fewest(0)
+    |> checksum(1, 2)
+  end
+
   def decode(data, width: width, height: height) do
     data
     |> String.to_integer()
     |> Integer.digits()
     |> Enum.chunk_every(width * height)
     |> Enum.map(&Enum.chunk_every(&1, width))
+  end
+
+  def count_digits(image) do
+    image
+    |> Enum.map(fn layer ->
+      layer
+      |> List.flatten()
+      |> Enum.reduce(%{}, fn digit, counts -> Map.update(counts, digit, 1, &(&1 + 1)) end)
+    end)
+  end
+
+  def fewest(layers, needle) do
+    Enum.min_by(layers, fn meta -> Map.get(meta, needle) end)
+  end
+
+  def checksum(counts, a, b) do
+    Map.get(counts, a) * Map.get(counts, b)
   end
 end
--- day08/test/day08_test.exs
+++ day08/test/day08_test.exs
@@ -6,5 +6,13 @@ defmodule Day08Test do
              [[1, 2, 3], [4, 5, 6]],
              [[7, 8, 9], [0, 1, 2]]
            ]
+
+    assert Day08.part1("123456789012", width: 3, height: 2) == 1
+  end
+
+  test "part 1 solution" do
+    input = File.read!("data/input.txt") |> String.trim_trailing("\n")
+
+    assert Day08.part1(input, width: 25, height: 6) == 2520
   end
 end
