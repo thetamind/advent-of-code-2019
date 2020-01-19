67a783f39b628bc6b9ce10b47d7a24ca3222e518
Matthew Boehlig <thetamind@gmail.com>
2020-01-19 14:59:14 -0600

    Day 10 parse input

--- day10/lib/day10.ex
+++ day10/lib/day10.ex
@@ -1,18 +1,36 @@
 defmodule Day10 do
-  @moduledoc """
-  Documentation for `Day10`.
-  """
+  @moduledoc false
 
-  @doc """
-  Hello world.
+  def parse(string) do
+    string
+    |> String.split("\n")
+    |> Enum.with_index()
+    |> Enum.flat_map(fn {line, y} ->
+      line
+      |> String.codepoints()
+      |> Enum.with_index()
+      |> Enum.reduce([], fn {char, x}, acc ->
+        case char do
+          "#" -> [{x, y} | acc]
+          "." -> acc
+        end
+      end)
+    end)
+  end
 
-  ## Examples
+  def load(string) do
+    string
+    |> parse()
+    |> Day10.Map.new()
+  end
+end
 
-      iex> Day10.hello()
-      :world
+defmodule Day10.Map do
+  def new(points) do
+    MapSet.new(points)
+  end
 
-  """
-  def hello do
-    :world
+  def member?(asteroids, x, y) do
+    MapSet.member?(asteroids, {x, y})
   end
 end
--- day10/test/day10_test.exs
+++ day10/test/day10_test.exs
@@ -1,8 +1,41 @@
 defmodule Day10Test do
   use ExUnit.Case
+
   doctest Day10
 
-  test "greets the world" do
-    assert Day10.hello() == :world
+  test "parse" do
+    input = ~S"""
+    .#..#
+    .....
+    #####
+    ....#
+    ...##
+    """
+
+    asteroids = Day10.parse(input)
+
+    assert Enum.member?(asteroids, {1, 0})
+    assert Enum.member?(asteroids, {4, 0})
+    assert Enum.member?(asteroids, {2, 2})
+    assert Enum.member?(asteroids, {4, 4})
+
+    refute Enum.member?(asteroids, {0, 0})
+    refute Enum.member?(asteroids, {9, 2})
+    refute Enum.member?(asteroids, {5, 5})
+  end
+
+  test "load" do
+    input = ~S"""
+    .#..#
+    .....
+    #####
+    ....#
+    ...##
+    """
+
+    map = Day10.load(input)
+
+    assert Day10.Map.member?(map, 3, 4)
+    refute Day10.Map.member?(map, 0, 0)
   end
 end
