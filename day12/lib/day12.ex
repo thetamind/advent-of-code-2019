43c2228f02294361261346f5fe48977f0c486440
Matthew Boehlig <thetamind@gmail.com>
2020-03-06 15:17:12 -0600

    Day 12 load simulation with Moon, Position, Velocity structs

--- day12/lib/day12.ex
+++ day12/lib/day12.ex
@@ -1,14 +1,69 @@
 defmodule Day12 do
   @moduledoc false
 
+  defmodule Position do
+    defstruct [:x, :y, :z]
+    @type t :: %__MODULE__{x: integer(), y: integer(), z: integer()}
+
+    def new({x, y, z}) do
+      %__MODULE__{x: x, y: y, z: z}
+    end
+  end
+
+  defmodule Velocity do
+    defstruct [:x, :y, :z]
+    @type t :: %__MODULE__{x: integer(), y: integer(), z: integer()}
+
+    def new({x, y, z}) do
+      %__MODULE__{x: x, y: y, z: z}
+    end
+
+    def zero() do
+      %__MODULE__{x: 0, y: 0, z: 0}
+    end
+  end
+
+  defmodule Moon do
+    @enforce_keys [:position, :velocity]
+    defstruct [:position, :velocity]
+
+    @type t :: %__MODULE__{
+            position: Position.t(),
+            velocity: Velocity.t()
+          }
+    def new(pos) do
+      %__MODULE__{position: Position.new(pos), velocity: Velocity.zero()}
+    end
+
+    def position(%{position: %{x: x, y: y, z: z}}), do: {x, y, z}
+    def velocity(%{velocity: %{x: x, y: y, z: z}}), do: {x, y, z}
+  end
+
+  defmodule Simulation do
+    defstruct step: 0, moons: []
+    @type t :: %__MODULE__{step: non_neg_integer(), moons: [Moon.t()]}
+
+    def new(positions) do
+      moons =
+        positions
+        |> Enum.map(&Moon.new/1)
+
+      %__MODULE__{step: 0, moons: moons}
+    end
+
+    def get_moon(sim, index) do
+      Enum.at(sim.moons, index)
+    end
+  end
+
   def load(input) do
     input
     |> String.split("\n", trim: true)
-    |> Enum.map(&to_moon/1)
+    |> Enum.map(&parse_line/1)
   end
 
-  @spec to_moon(binary) :: {integer(), integer(), integer()}
-  def to_moon(line) do
+  @spec parse_line(binary) :: {integer(), integer(), integer()}
+  defp parse_line(line) do
     line
     |> String.split(["<", ">", ","])
     |> Enum.map(&String.trim/1)
--- day12/test/day12_test.exs
+++ day12/test/day12_test.exs
@@ -2,16 +2,17 @@ defmodule Day12Test do
   use ExUnit.Case
 
   import Day12
+  alias Day12.{Moon, Simulation}
+
+  @example1 ~S"""
+  <x=-1, y=0, z=2>
+  <x=2, y=-10, z=-7>
+  <x=4, y=-8, z=8>
+  <x=3, y=5, z=-1>
+  """
 
   describe "example 1" do
-    test "moon positions" do
-      input = ~S"""
-      <x=-1, y=0, z=2>
-      <x=2, y=-10, z=-7>
-      <x=4, y=-8, z=8>
-      <x=3, y=5, z=-1>
-      """
-
+    test "parse moon positions" do
       expected = [
         {-1, 0, 2},
         {2, -10, -7},
@@ -19,7 +20,16 @@ defmodule Day12Test do
         {3, 5, -1}
       ]
 
-      assert load(input) == expected
+      assert load(@example1) == expected
+    end
+
+    test "load simulation" do
+      positions = load(@example1)
+
+      sim = Simulation.new(positions)
+
+      assert Simulation.get_moon(sim, 0) |> Moon.position() == {-1, 0, 2}
+      assert Simulation.get_moon(sim, 3) |> Moon.velocity() == {0, 0, 0}
     end
   end
 end
